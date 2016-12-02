e55650b7791bf9eea0caaaeb844a45bb26169843
ismaelsb <ismael.santosb@gmail.com>
2016-12-02 18:47:56 +0000

    Sammon mapping instead of cmdscale (preserves distances better). A new distance function based on correlations. Plot the strongest edges

--- WhatsAppGraphBuckets.R
+++ WhatsAppGraphBuckets.R
@@ -107,7 +107,7 @@ diag(distCorrm)<-NA
 
 heatmap(distCorrm, col=colorpalette, labRow=names, labCol=names)
 
-cmdC <- cmdscale(distCorr, k = 18, eig=T, add=T)
+cmdC <- cmdscale(distCorr, k = length(names)-1, eig=T, add=T)
 km.out <- kmeans(cmdC$points,centers=ngroups,nstart=15)
 km.out$cluster
 indexcoords <- c(1,2)
@@ -150,4 +150,81 @@ plot(hc,cex=.7, labels=names)
 distBm <- as.matrix(distB)
 diag(distBm)<-NA
 heatmap(distBm, col=colorpalette, labRow=names, labCol=names)
+
+
+#Sammon Mapping (preserves distances better)
+library(MASS)
+SammonCoords <- sammon(distB)
+plot(SammonCoords$points)
+#color tree clusters
+text(SammonCoords$points,labels=names, pos=4, cex=0.6, col=Mgroups)
+#color kmeans clusters
+text(SammonCoords$points,labels=names, pos=4, cex=0.6, col=km.out$cluster)
+
+
+kmSammon <- kmeans(SammonCoords$points,centers=ngroups,nstart=15)
+kmSammon$cluster
+text(SammonCoords$points,labels=names, pos=4, cex=0.6, col=kmSammon$cluster)
+
+
+SammonCoordsCorr <- sammon(dist(cor(B)))
+plot(SammonCoordsCorr$points)
+#color tree clusters
+text(SammonCoordsCorr$points,labels=names, pos=4, cex=0.6, col=Mgroups)
+#color kmeans clusters
+text(SammonCoordsCorr$points,labels=names, pos=4, cex=0.6, col=km.out$cluster)
+
+
+kmSammonCorr <- kmeans(SammonCoordsCorr$points,centers=ngroups,nstart=15)
+kmSammonCorr$cluster
+text(SammonCoordsCorr$points,labels=names, pos=4, cex=0.6, col=kmSammonCorr$cluster)
+
+
+hc <- hclust(dist(cor(B)), method="complete")
+plot(hc,cex=.7, labels=names) #dendogram for simultaneity in conversation
+
+
+
+####Sammon Mapping with a distance based on correlations
+
+#Corrdist <- (1-cor(B))/2
+Corrdist <- tan(pi/2*(1-cor(B))/2) # maps cor 1 to dist 0 and cor -1 to +inf
+
+SammonCoordsCorrD <- sammon(Corrdist)
+
+hc <- hclust(as.dist(Corrdist), method="complete")
+plot(hc,cex=.7, labels=names) #dendogram for simultaneity in conversation
+
+
+plot(SammonCoordsCorrD$points, col="gray",
+     xlab="", ylab="", bty="n", ann=F, xaxt='n',yaxt='n',
+     xlim=c(min(SammonCoordsCorrD$points[,1])-.05,max(SammonCoordsCorrD$points[,1])+.05),
+     ylim=c(min(SammonCoordsCorrD$points[,2])-.05,max(SammonCoordsCorrD$points[,2])+.05)#,
+     )
+#color tree clusters
+text(SammonCoordsCorrD$points,labels=names, pos=1, cex=0.6, col=Mgroups)
+#color kmeans clusters
+text(SammonCoordsCorrD$points,labels=names, pos=1, cex=0.6, col=km.out$cluster)
+
+
+kmSammonCorrD <- kmeans(SammonCoordsCorrD$points,centers=ngroups,nstart=15)
+kmSammonCorrD$cluster
+text(SammonCoordsCorrD$points,labels=names, pos=1, cex=0.6, col=kmSammon$cluster)
+
+
+MgroupsCorr <- cutree(hc, k=ngroups)
+text(SammonCoordsCorrD$points,labels=names, pos=1, cex=0.6, col=MgroupsCorr)
+
+#plot the strongest edges
+
+for (i in 1:length(names)){
+  for (j in i:length(names)){
+    if (Corrdist[i,j]<quantile(Corrdist,1/3)) {
+      segments(SammonCoordsCorrD$points[i,1],SammonCoordsCorrD$points[i,2],
+               SammonCoordsCorrD$points[j,1],SammonCoordsCorrD$points[j,2],
+               lwd=1, lty="dashed", col="gray")
+    }
+  }
+}
+
 
